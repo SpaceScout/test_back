9d0a3175919968500c42481e62a98d75c8b8d8fa
SpaceScout <nelindanila7@gmail.com>
2022-09-20 17:21:59 +0300

    1

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 
 
-const PORT = "4999"
+const PORT = "5000"
 const express = require("express")
+const {json} = require("express");
 const app = express()
-const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: "4999",
-    user: "root",
-    database: "firstDB",
-    password: "123"
-});
-
-connection.connect(function(err){
-    if (err) {
-        return console.error("Ошибка: " + err.message);
-    }
-    else{
-        console.log("Подключение к серверу MySQL успешно установлено");
-    }
-});
+app.use(json())
 
 app.listen(PORT, () => console.log("писька"))

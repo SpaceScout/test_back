a16860ae922892e75eb0007fd7fe053b0fd0c093
SpaceScout <nelindanila7@gmail.com>
2022-11-01 00:36:58 +0300

    1

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,26 +1,43 @@
-const User = require('../models/user.model')
-const Role = require('../models/role.model')
+const user_model = require('../models/user.model')
+const role_model = require('../models/role.model')
+const jwt = require('jsonwebtoken')
+const token_model = require('../models/token.model')
 
 class AuthController{
     async registration(req, res)
     {
         try {
+
             const name = req.body.name
             const password = req.body.password
-            const candidate = await User.findOne({name})
+            console.log(name)
+            console.log(password)
+            console.log("хуййй")
+            const candidate = await user_model.findOne({where:{user_name: name}})
             if (candidate)
             {
                 return res.status(400).json({message:'Такой юзер уже есть'})
             }
             console.log(name)
             console.log(password)
-            const new_user = new User
+            const new_user = new user_model
             ({
                 user_name: name,
                 user_password: password
             })
             await new_user.save()
             return res.status(200).json({message:'уююююююююююют'})
+            payload = ""
+            const accessToken = jwt.sign(payload, process.env.jwt_access_secret, {expiresIn:'1m'})
+            const refreshToken = jwt.sign(payload, process.env.jwt_refresh_secret, {expiresIn:'2m'})
+            userId = ""
+            const tokenData = await token_model.findOne({where:{userUserId:userId}})
+            if(tokenData){
+                tokenData.refreshToken = refreshToken
+                tokenData.save()
+            }
+            const token = await token_model.create({userUserId:userId, refreshToken})
+
         }catch (e){
             return res.status(500).message("ошибочкаааааа")
         }
